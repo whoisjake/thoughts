8f3e9fb73647b5e919d189cc19dba77172c90164
Jake Good <jake@developstuff.com>
2009-05-03 17:01:00 -0500

    Spam specs

--- models/comment.rb
+++ models/comment.rb
@@ -2,7 +2,6 @@ class Comment < Sequel::Model
   many_to_one :post
   
   include Splam
-  
   splammable :body do |splam|
     splam.threshold = 40
     splam.rules = [:bad_words, :html, :bbcode, :href, :chinese, :line_length, :russian]
--- spec/models/comments.rb
+++ spec/models/comments.rb
@@ -42,7 +42,13 @@ describe Comment do
     @comment.ip_address = "127.0.0.1"
     @comment.website = "http://thoughtstoblog.com"
     @comment.splam?.should == false
-    @comment.splam_score.should < 40
+    @comment.splam_score.should == 0
+  end
+  
+  it "should filter out bad words." do
+    @comment.body = "buy my cheap sexy gay porn viagra"
+    @comment.splam?.should == true
+    @comment.splam_score.should == 54 # buy(4) sexy(10,10) gay(10) porn(10) viagra(10)
   end
 
 end
