380c7e6f8bf60002de369dc1101d31f727463deb
Jake Good <jake@developstuff.com>
2009-05-04 17:11:12 -0500

    Added some more routes and actions

--- routes.rb
+++ routes.rb
@@ -4,46 +4,71 @@ end
 
 get '/admin' do
   # show activity
+  erb :admin
 end
 
 get '/admin/posts' do
   # show posts
+  @posts = Post.filter(:published => true).order(:published_at.desc).limit(10)
+  erb :admin_posts
 end
 
 get '/admin/posts/:id' do
-  # show post
+  @post = Post[params[:id].to_i]
+  halt 404, "Post not found" unless @post
+  redirect_to "/#{@post.permalink}"
 end
 
 post '/admin/posts' do
   # create post
+  redirect_to '/admin/posts'
 end
 
 put '/admin/posts/:id' do
-  # edit post
+  @post = Post[params[:id].to_i]
+  halt 404, "Post not found" unless @post
+  @post.body = params[:post][:body]
+  @post.save
+  redirect_to "/#{@post.permalink}"
 end
 
 delete '/admin/posts/:id' do
-  # delete post
+  @post = Post[params[:id].to_i]
+  halt 404, "Post not found" unless @post
+  @post.delete
+  redirect_to '/admin/posts'
 end
 
 get '/admin/comments' do
   # show comments
+  @comments = Comment.all.order(:created_at.desc).limit(10)
+  erb :admin_comments
 end
 
 get '/admin/comments/:id' do
-  # show comment
-end
-
-post '/admin/comments' do
-  # create comment
+  @comment = Comment[params[:id].to_i]
+  halt 404, "Comment not found" unless @comment
+  erb :admin_comment
 end
 
 put '/admin/comments/:id' do
-  # edit comment
+  @comment = Comment[params[:id].to_i]
+  halt 404, "Comment not found" unless @comment
+  
+  @comment.body = params[:comment][:body]
+  @comment.name = params[:comment][:name]
+  @comment.email = params[:comment][:email]
+  @comment.website = params[:comment][:website]
+  @comment.save
+  
+  redirect_to "/admin/comments/#{@comment.id}"
 end
 
 delete '/admin/comments/:id' do
-  # delete comment
+  @comment = Comment[params[:id].to_i]
+  halt 404, "Comment not found" unless @comment
+  @comment.delete
+  redirect_to '/admin/comments'
 end
 
 get '/' do
@@ -74,7 +99,13 @@ get %r{\A\/([0-9]{4})\/([0-9]{2})\/([0-9]{2})\/?} do
 end
 
 post '/*/comments' do
-  # creates a comment
+  @post = Post.filter('published = ? AND permalink = ?', true, params[:splat][0]).first
+  halt 404, "Post not found" unless @post
+  
+  @comment = Comment.new(params[:comment])
+  @comment.save
+  
+  redirect_to "/#{@post.permalink}"
 end
 
 get '/*' do
